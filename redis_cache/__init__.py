f18f64bd34af3c7c2c1554a5c0bfed2b1a36af66
kunsam002 <kunsam002@gmail.com>
2023-08-08 13:11:41 -0400

    Artifactory build setup

--- redis_cache/__init__.py
+++ redis_cache/__init__.py
@@ -1,3 +1,5 @@
 from redis_cache.backends.single import RedisCache
 from redis_cache.backends.multiple import ShardedRedisCache
 from redis_cache.backends.dummy import RedisDummyCache
+
+__version__ = "v2.1.1-vndly-0.0.1"
--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
+import os
+import re
+
 from setuptools import setup
+
+
+def get_version(filename):
+    path = os.path.join(os.path.dirname(__file__), filename)
+    with open(path, encoding="utf-8") as handle:
+        content = handle.read()
+    return re.search(r'__version__ = "([^"]+)"', content).group(1)
+
 
 setup(
     name="django-redis-cache",
     url="http://github.com/sebleier/django-redis-cache/",
     author="Sean Bleier",
     author_email="sebleier@gmail.com",
-    version="2.1.1",
+    version=get_version('redis_cache/__init__.py'),
     license="BSD",
     packages=["redis_cache", "redis_cache.backends"],
     description="Redis Cache Backend for Django",
