a44ab781d863a0853cd865c20688a9a2e71f8e0d
Nick Scavelli <nick.scavelli@workday.com>
2025-03-21 18:24:57 -0400

    Python 3.12 test

--- redis_cache/__init__.py
+++ redis_cache/__init__.py
@@ -2,4 +2,4 @@ from redis_cache.backends.single import RedisCache
 from redis_cache.backends.multiple import ShardedRedisCache
 from redis_cache.backends.dummy import RedisDummyCache
 
-__version__ = "v2.1.1-vndly-0.0.2"
+__version__ = "2.1.1+vndly-0.0.3"
